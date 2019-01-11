55b279c320745eccc049fde71e48bd881e61ad81
qibebt-bioinfo <46369225+qibebt-bioinfo@users.noreply.github.com>
2019-01-11 19:03:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ from setuptools import setup, find_packages
 setup(
     name="q2-metastorms",
     packages=find_packages(),
-    author="",   #### Please correct the author information
-    author_email="",
-    description="",
+    author="Gongchao Jing",   #### Please correct the author information
+    author_email="jinggc@qibebt.ac.cn",
+    description="Search engine designed to efficiently search a database of microbiome samples and identify similar samples based on phylogenetic relatedness",
     license='BSD-3-Clause',
-    url="na",   ### This should be the URL for MSE or its github page etc etc
+    url="http://mse.single-cell.cn",   ### This should be the URL for MSE or its github page etc etc
     entry_points={
         'qiime2.plugins': ['q2-type-example=q2_metastorms.plugin_setup:plugin']
     },
