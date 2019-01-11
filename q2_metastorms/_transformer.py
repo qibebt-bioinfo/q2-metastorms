93399cb5b8c4cac6b0e7b3fb4b53da8569882cfb
qibebt-bioinfo <46369225+qibebt-bioinfo@users.noreply.github.com>
2019-01-11 20:03:37 +0800

    Update _transformer.py

--- q2_metastorms/_transformer.py
+++ q2_metastorms/_transformer.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2018, Meta-Storms development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 import shutil
 
 from .plugin_setup import plugin
